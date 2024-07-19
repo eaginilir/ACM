c376355749bfe2bdf267338c7b27bdf43058bd03
22302 <22302@DESKTOP-O36K87E>
2024-07-19 22:37:21 +0800

    还没写完

--- 高精度加法和乘法/高精度加法和乘法.cpp
+++ 高精度加法和乘法/高精度加法和乘法.cpp
@@ -1,7 +1,40 @@
 ﻿#include<iostream>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+
 using namespace std;
+
+class big_int
+{
+public:
+	big_int() :integer("\0") {}
+
+	big_int(string num):integer(num)
+	{
+		reverse(integer.begin(), integer.end());
+	}
+
+	big_int operator+(big_int& num)
+	{
+		string result;
+		int max_size = max(this->integer.size(), num.integer.size());
+		int remnant = 0;//进数
+		
+		
+
+	}
+
+private:
+	string integer;
+
+
+};
+
+
 int main()
 {
-	cout << "hello" << endl;
+	
 	return 0;
 }
